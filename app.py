c5f44a6328a15fe7de89d9456eaefaf99d858c9d
danzzim <43137909+danzzim@users.noreply.github.com>
2020-11-20 23:12:25 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -26,6 +26,14 @@ def predict():
     prediction = int(prediction.Label[0])
     return render_template('home.html', pred = 'Expected Wine Quality will be {}'.format(prediction))
 
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
